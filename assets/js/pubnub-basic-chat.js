b18bf0f1614ed0233d4eff70faf59e122bdb7f71
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-29 18:40:40 -0400

    OOP method for user to assign role

--- assets/js/pubnub-basic-chat.js
+++ assets/js/pubnub-basic-chat.js
@@ -2,6 +2,20 @@
 	var pubnub;
 	var channel = "holgov_test1";
 
+	var User = function(user_id, org_object) {
+		this.uuid = user_id;
+		this.assignRole = function() {
+			// random role from org_object
+			var random_role_index = Math.round((Math.random())*(org_object.length-1));
+			var assigned_role = org_object[random_role_index];
+			return assigned_role;
+		}
+	}
+
+	var haley = new User(300, org_data.roles);
+
+	console.log(haley.assignRole());
+
 	var pubnub = PUBNUB.init({
 		publish_key: 'pub-c-965c2c1a-af86-4cdd-bfd9-7d390b4d85d3',
 		subscribe_key: 'sub-c-6037cec6-54ff-11e6-bd9c-0619f8945a4f',
@@ -19,7 +33,7 @@
 	pubnub.subscribe({
 		channel: channel,
 		presence: function(m) {
-			console.log(m);
+			// console.log(m);
 		},
 		state: {
 			name: 'presence-tutorial-user',
@@ -34,6 +48,13 @@
 		}
 	});
 
+	pubnub.here_now({
+		channel: channel,
+		callback: function(m) {
+			// console.log(m);
+		}
+	})
+
 	function publish() {
 		pubnub.publish({
 			channel: channel,
@@ -41,7 +62,7 @@
 				"text": checkin_input.val()
 			},
 			callback: function(m) {
-				console.log(m)
+				// console.log(m)
 			}
 		});
 	}
