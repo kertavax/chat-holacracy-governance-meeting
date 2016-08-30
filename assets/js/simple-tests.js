9073f523daf6756305d2ffc86bbe25b48c0f7c4d
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-30 18:21:01 -0400

    assign role, remove said role from data to avoid duplication of role assignments

--- assets/js/simple-tests.js
+++ assets/js/simple-tests.js
@@ -13,7 +13,8 @@ var organization_data = {
 			"name": "Marketing Maven",
 			"purpose": "Help the world to know and love Better Training Videos and our products and services",
 			"accountabilities": ["Defining and implementing coordinated strategies and initiatives to spread awareness of the products/services we offer to help people with Better Training Videos"],
-			"domains": []
+			"domains": [],
+			"tension": "There is a need of copywriting for various outlets"
 		}
 	]
 };
@@ -33,13 +34,24 @@ var User = function(org) {
 var member_list = [];
 
 var getRole = function(org) {
-	// random role from organization_data
-	var random_role_index = Math.round(Math.random());
-	var assigned_role = org[random_role_index];
+
+	var random_role_index, assigned_role, q;
+
+	// check number of roles
+	var roles_total = org.length;
+
+	if (roles_total == 1) {
+		random_role_index = 0;
+	} else {
+		random_role_index = Math.round(Math.random());	
+	}
+	
+	// random role from organization_data based on number of roles
+	assigned_role = org[random_role_index];
 	console.log("You got the role:", assigned_role);
 
 	// remove the randomly selected role from organization_data
-	var q = organization_data.roles.indexOf(assigned_role);
+	q = organization_data.roles.indexOf(assigned_role);
 	organization_data.roles.splice(q, 1);
 
 	console.log("The role:", assigned_role.name, "should be removed from:", organization_data.roles);
