51d463721d4f578668f4a0b47c5b2ef3277ee610
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-31 22:14:13 -0400

    variable scope is different between users, ie., they all have their own versions of var count. Might look into angular-pubnub and two-way data-binding

--- assets/js/simple-tests.js
+++ assets/js/simple-tests.js
@@ -1,6 +1,11 @@
-// Establish PUBNUB instance and channel
+var app = angular.module("BidirectionalApp", ["pubnub.angular.service"]);
 
-var channel = "holgov_test1";
+app.controller("MainCtrl", function($scope, Pubnub) {
+	
+})
+
+var channel = "holgov1";
+var count = 0;
 
 var pubnub = PUBNUB.init({
 	publish_key: 'pub-c-965c2c1a-af86-4cdd-bfd9-7d390b4d85d3',
@@ -11,106 +16,37 @@ var pubnub = PUBNUB.init({
 	}
 });
 
-
-// Organization JSON
-
-var organization_data = {
-	"organization": "Better Training Videos",
-	"roles": [{
-			"name": "Social Media Butterfly",
-			"purpose": "Pollinate the web with information regarding our training videos",
-			"accountabilities": ["Creating or sourcing quick-to-consume content of interest to Better Training Videos’s market and posting to social media channels", "Responding to misinformation about Better Training Videos on social media channels", "Monitoring social media channels and routing or responding to relevant follow-up comments on posted content"],
-			"domains": ["Better Training Videos’s social media accounts"],
-			"tension": "People online have been stating that the training videos don’t accurately represent our offers made in our marketing materials"
-		},{
-			"name": "Marketing Maven",
-			"purpose": "Help the world to know and love Better Training Videos and our products and services",
-			"accountabilities": ["Defining and implementing coordinated strategies and initiatives to spread awareness of the products/services we offer to help people with Better Training Videos"],
-			"domains": [],
-			"tension": "There is a need of copywriting for various outlets"
-		}
-	]
-};
-
-
-// List of users
-
-var member_list = [];
-
-
-// State initial data
-
-var present_data = function() {
+var volume_change = function(change) {	
+	var change = (change == "up") ? count++ : count--;
 	pubnub.publish({
 		channel: channel,
 		message: {
-			"content": "This is the organization_data " + JSON.stringify(organization_data)
+			counter: change
 		}
 	});
-}
+};
 
+$(".volUp").on("click", function() {
+	volume_change("up");
+});
 
-// Create a user, assign a role from data, remove said role from data (to avoid duplicate role assignments)
-
-var getRole = function(org) {
-
-	var random_role_index, assigned_role, q;
-
-	// check number of roles
-	var roles_total = org.length;
-	console.log(roles_total);
-
-	if (roles_total == 1) {
-		random_role_index = 0;
-	}
-	else if (roles_total < 1) {
-		return console.log("No roles to assign. Consider asking the other roles to create additional roles at next Governance Meeting");
-	}
-	else {
-		random_role_index = Math.round(Math.random());	
-	}
-	
-	// random role from organization_data based on number of roles
-	assigned_role = org[random_role_index];
-
-	// remove the randomly selected role from organization_data
-	q = organization_data.roles.indexOf(assigned_role);
-	organization_data.roles.splice(q, 1);
-
-	pubnub.publish({
-		channel: channel,
-		message: {
-			"content": "You got the role of " + assigned_role.name
-		}
-	})
-
-	// push the selected role into member_list array
-	return member_list.push(assigned_role);
-}
+$(".volDown").on("click", function() {
+	volume_change("down");
+});
 
 pubnub.subscribe({
 	channel: channel,
 	message: function(m) {
-		console.log(m.content);
-	},
-	presence: function(p) {
-		// console.log(p);
+		$(".output").append("p").text(m.counter);
 	}
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pubnub.history({
+	channel: channel,
+	callback: function(h) {
+		var historylength = h[0].length;
+		for (var i = 0; i < historylength; i++) {
+			$(".history").find("ul").html("<li>" + h[0][i].counter + "</li>")
+		};
+	}
+})
